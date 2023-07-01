7857edf94346e56fbf5aae00b6a0d8750edc8b5f
[Bit-Zero0] <[3153366821@qq.com]>
2023-07-01 16:04:18 +0800

    新增宏定义日志, 日志翻滚功能

--- BZ_log.h
+++ BZ_log.h
@@ -12,6 +12,16 @@ using namespace std;
 namespace BZ_LOG
 {
 
+#define debug(format, ...) Logger::instance()->log(Logger::DEBUG, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+#define info(format, ...) Logger::instance()->log(Logger::INFO, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+#define warn(format, ...) Logger::instance()->log(Logger::WARN, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+#define error(format, ...) Logger::instance()->log(Logger::ERROR, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
+#define fatal(format, ...) Logger::instance()->log(Logger::FATAL, __FILE__, __LINE__, format, ##__VA_ARGS__);
+
     class Logger
     {
     public:
@@ -26,7 +36,8 @@ namespace BZ_LOG
         };
 
         void open(const string &filename);
-        void max(int byte);
+        void max(int kb);
+        void level(Level level);
         void log(Level level, const string &file, int line, const string &format, ...);
 
         static Logger *instance();
@@ -37,11 +48,15 @@ namespace BZ_LOG
 
         void close();
 
+        void rotate();
+
         const static vector<string> log_level;
 
         Level def_level;     // 默认日志级别
         string log_filename; // 存储日志的文件
         ofstream f_out;      // 控制日志走向的输出流
+        int log_max;         // 日志默认大小
+        int log_len;         // 记录日志文件的长度, 方便后续日志文件转存.
 
         static Logger *log_instance;
     };
@@ -52,8 +67,23 @@ using namespace BZ_LOG;
 Logger *Logger::log_instance = nullptr;
 
 Logger::Logger()
-    : def_level(DEBUG)
+    : def_level(DEBUG), log_max(10240), log_len(0) // 默认每个日志大小为10kb
 {
+}
+
+Logger::~Logger()
+{
+    close();
+}
+
+void Logger::max(int kb)
+{
+    log_max = kb * 1024;
+}
+
+void Logger::level(Level level)
+{
+    def_level = level;
 }
 
 Logger *Logger::instance()
@@ -71,6 +101,13 @@ void Logger::open(const string &filename)
     {
         throw std::logic_error("open log file failed" + filename);
     }
+
+    // seekp 和 tellp 函数
+    // 输出文件流保留指向下一次写入数据的位置的内部指针。
+    // seekp 成员函数将设置此指针，从而提供随机访问磁盘文件输出。
+    // tellp 成员函数将返回文件位置。
+    f_out.seekp(0, ios::end); // 将文件指针移到文件末尾
+    log_len = f_out.tellp();
 }
 
 void Logger::close()
@@ -87,6 +124,12 @@ const vector<string> Logger::log_level = {
 
 void Logger::log(Level level, const string &file, int line, const string &format, ...)
 {
+
+    if (level < def_level)
+    {
+        return;
+    }
+
     if (!f_out.is_open())
     {
         throw logic_error("open log file failed" + log_filename);
@@ -112,6 +155,7 @@ void Logger::log(Level level, const string &file, int line, const string &format
         snprintf(buffer, len + 1, fmt, time, log_level[level].c_str(), file.c_str(), line);
         buffer[len] = 0;
         f_out << buffer;
+        log_len += len;
         delete buffer;
     }
 
@@ -128,9 +172,36 @@ void Logger::log(Level level, const string &file, int line, const string &format
         va_end(arg);
         content[len] = 0;
         f_out << content;
+        log_len += len;
         delete content;
     }
 
     f_out << "\n";
     f_out.flush();
+
+    if (log_max > 0 && log_len >= log_max)
+    {
+        rotate();
+    }
+}
+
+void Logger::rotate()
+{
+    close();
+    time_t timestamp = time(nullptr);
+    tm *tm = localtime(&timestamp);
+
+    char time[32];
+    memset(time, 0, sizeof(time));
+    strftime(time, sizeof(time), "%Y-%m-%d %H:%M:%S", tm);
+    string filename = log_filename + time;
+
+    // rename函数介绍
+    // int rename ( const char * oldname, const char * newname );
+    // 将oldname的文件名改为newname的文件名, 成功返回 0 , 不成功返回非0
+    if (rename(log_filename.c_str(), filename.c_str()) != 0)
+    {
+        logic_error("rename log file failed " + string(strerror(errno)));
+    }
+    open(log_filename);
 }
