353e1291a5b1796bcf068810fbf68944edfb7cbe
[Bit-Zero0] <[3153366821@qq.com]>
2023-07-01 16:05:11 +0800

    使用案例

--- main.cpp
+++ main.cpp
@@ -5,9 +5,20 @@ int main()
 {
     Logger::instance()->open("./log.log");
 
+    Logger::instance()->max(5); // 设置日志文件最大容量, 单位为kb, 默认为10kb
+    // Logger::instance()->level(Logger::WARN); // 设置级别, 默认为DEBUG
+
     Logger::instance()->log(Logger::DEBUG, "bz-in", 8, "hello world");
-    Logger::instance()->log(Logger::DEBUG, "hello bz", 8, "age=%d , name=%s", 18, "bit-zero");
     Logger::instance()->log(Logger::INFO, "bz-in", 20, "hello");
+    Logger::instance()->log(Logger::WARN, "hello bz", 8, "age=%d , name=%s", 18, "bit-zero");
+    Logger::instance()->log(Logger::ERROR, "hello bz", 8, "age=%d , name=%s", 18, "bit-zero");
+    Logger::instance()->log(Logger::FATAL, "hello bz", 8, "age=%d , name=%s", 18, "bit-zero");
+
+    debug("测试1: %d:%d", 13, 15);
+    info("测试1: %d:%d", 13, 15);
+    warn("测试1: %d:%d", 13, 15);
+    error("测试1: %d:%d", 13, 15);
+    fatal("测试1: %d:%d", 13, 15);
 
     return 0;
 }
